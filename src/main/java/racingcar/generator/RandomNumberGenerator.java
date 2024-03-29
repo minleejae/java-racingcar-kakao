6980b3f6f1352adc5fe14f95c5626d5a68045ce4
minleejae <mmj9808@naver.com>
2024-03-29 11:17:23 +0900

    refactor: 상수 관리, 마지막 newline 추가

--- src/main/java/racingcar/generator/RandomNumberGenerator.java
+++ src/main/java/racingcar/generator/RandomNumberGenerator.java
@@ -3,11 +3,12 @@ package racingcar.generator;
 import java.util.Random;
 
 public class RandomNumberGenerator implements NumberGenerator {
+    private static final int UPPER_BOUND = 10;
     private final Random random = new Random();
 
     @Override
     public int generate() {
-        return random.nextInt(10);
+        return random.nextInt(UPPER_BOUND);
     }
 }
 
