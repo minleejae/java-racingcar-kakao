c9b6aae759591eedc3816404e708be052810674a
minleejae <mmj9808@naver.com>
2024-03-31 22:31:26 +0900

    refactor: 메서드 순서 재배치

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -1,22 +1,14 @@
 package racingcar.model;
 
 import racingcar.generator.NumberGenerator;
-import racingcar.generator.RandomNumberGenerator;
 
 public class RacingCar {
     private static final int MAX_NAME_LENGTH = 5;
     private static final int MIN_MOVE_THRESHOLD = 4;
     private static final int MAX_MOVE_THRESHOLD = 9;
-    private NumberGenerator numberGenerator;
-    private String name;
+    private final NumberGenerator numberGenerator;
+    private final String name;
     private int moves = 0;
-
-    public RacingCar() {
-    }
-
-    public RacingCar(String name) {
-        this(name, new RandomNumberGenerator());
-    }
 
     public RacingCar(String name, NumberGenerator numberGenerator) {
         validateName(name);
@@ -24,15 +16,21 @@ public class RacingCar {
         this.numberGenerator = numberGenerator;
     }
 
-    private void getNumAndMove(int num) {
-        if (isMove(num)) {
-            this.moves++;
+    private void validateName(String name) {
+        if (name == null || name.isEmpty() || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름이 적절하지 않습니다.");
         }
     }
 
     public void move() {
         int num = numberGenerator.generate();
         getNumAndMove(num);
+    }
+
+    private void getNumAndMove(int num) {
+        if (isMove(num)) {
+            this.moves++;
+        }
     }
 
     private boolean isMove(int num) {
@@ -49,11 +47,5 @@ public class RacingCar {
 
     public boolean isAt(int moves) {
         return this.moves == moves;
-    }
-
-    private void validateName(String name) {
-        if (name == null || name.isEmpty() || name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("자동차 이름이 적절하지 않습니다.");
-        }
     }
 }
